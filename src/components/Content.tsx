7bf154e913b23fc913e098e2f78846e3eb701645
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-05-13 08:31:49 -0300

    componente Content.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
-  // Complete aqui
+import { Button } from "../components/Button";
+
+import "../styles/content.scss";
+interface GenreResponseProps {
+  genres: {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }[];
+}
+
+export function Content(props: any) {
+  const { genres }: GenreResponseProps = props;
+  const { handleClickButton } = props;
+  const { selectedGenreId } = props;
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
